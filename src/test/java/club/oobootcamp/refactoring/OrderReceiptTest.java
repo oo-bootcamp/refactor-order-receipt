f72acb9537279476914d95333099ceed74cf581f
YuanShenjian <sjyuan@thoughtworks.com>
2018-07-01 09:01:42 +0800

    Init project
    Signed-off-by: YuanShenjian <sjyuan@thoughtworks.com>

---
+++ src/test/java/club/oobootcamp/refactoring/OrderReceiptTest.java
@@ -0,0 +1,40 @@
+package club.oobootcamp.refactoring;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class OrderReceiptTest {
+    @Test
+    public void should_print_customer_information_on_order() {
+        Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>());
+        OrderReceipt receipt = new OrderReceipt(order);
+
+        String output = receipt.printReceipt();
+
+        assertThat(output).contains("Mr X");
+        assertThat(output).contains("Chicago, 60601");
+    }
+
+    @Test
+    public void should_print_line_item_and_sales_tax_information() {
+        ArrayList<LineItem> lineItems = new ArrayList<LineItem>() {{
+            add(new LineItem("milk", 10.0, 2));
+            add(new LineItem("biscuits", 5.0, 5));
+            add(new LineItem("chocolate", 20.0, 1));
+        }};
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, lineItems));
+
+        String output = receipt.printReceipt();
+
+        assertThat(output).contains("milk\t10.0\t2\t20.0");
+        assertThat(output).contains("biscuits\t5.0\t5\t25.0\n");
+        assertThat(output).contains("chocolate\t20.0\t1\t20.0\n");
+        assertThat(output).contains("Sales Tax\t6.5");
+        assertThat(output).contains("Total Amount\t71.5");
+    }
+
+}
